d09c3c6d5c524e75b86ccec2c71f98039ddc8f35
Bojan Mitrovic <71645877+Bojan-Mitrovic@users.noreply.github.com>
2021-01-22 13:11:23 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const rockContainer = document.querySelector('#rockcontainer');
 const part1 = document.querySelector('.game-part1');
 const part2 = document.querySelector('.game-part2');
 const part3 = document.querySelector('.game-part3');
-const housePick = document.querySelector('#house-pick');
 // signs selected
 const player = document.querySelector('#player-picked');
+const player2 = document.querySelector('#player-picked2');
 const paper = document.querySelector('.game-paper');
 const rock = document.querySelector('.game-rock');
 const scissors = document.querySelector('.game-scissors');
+const housePick = document.querySelector('#house-pick');
+
 
 const signList = [];
 let random = Math.floor(Math.random() * 3);
@@ -21,21 +23,26 @@ function choosePaper() {
     rockContainer.style.display = 'none';
     part1.style.display = 'flex';
     player.classList.add('game-paper');
+    player2.classList.add('game-paper');
+
 
     setTimeout(function() {
 
-
         if (random == 1) {
-            housePick.classList.remove('.empty-spot');
-            housePick.classList.add('.base-class', '.game-scissors');
-        }
-        if (random == 2) {
-            housePick.classList.remove('.empty-spot');
-            housePick.classList.add('.base-class', '.game-rock');
+            housePick.classList.add('.game-scissors');
+            part1.style.display = 'none';
+            part2.style.display = 'flex'
+        } else if (random == 2) {
+            housePick.classList.add('.game-rock');
+            part1.style.display = 'none';
+            part2.style.display = 'flex'
         } else {
-            housePick.classList.remove('.empty-spot');
-            housePick.classList.add('.base-class', '.game-paper');
+            housePick.classList.add('.game-paper');
+            part1.style.display = 'none';
+            part2.style.display = 'flex'
         }
+
+        ;
 
     }, 2000);
 }
@@ -55,17 +62,4 @@ function chooseRock() {
 
 }
 
-// computer pick
-
-function housePick() {
-    let random = Math.floor(Math.random() * 3);
-
-    if (random = 0) {
-        housePick.classList.add('.base-class', '.game-scissors');
-    } else if (random = 1) {
-        housePick.classList.add('.base-class', '.game-rock');
-    } else {
-        housePick.classList.add('.base-class', '.game-paper');
-    }
-
-}
+//
