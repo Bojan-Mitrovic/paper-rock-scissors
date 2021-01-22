8c5cfcb13f5da0d11920e64e1108fb738a5ddfc5
Bojan Mitrovic <71645877+Bojan-Mitrovic@users.noreply.github.com>
2021-01-22 10:43:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ const rockContainer = document.querySelector('#rockcontainer');
 const part1 = document.querySelector('.game-part1');
 const part2 = document.querySelector('.game-part2');
 const part3 = document.querySelector('.game-part3');
-
+const housePick = document.querySelector('.the-house-pick');
 // signs selected
-const player = document.querySelector('#player-picked')
+const player = document.querySelector('#player-picked');
+const paper = document.querySelector('.game-paper');
+const rock = document.querySelector('.game-rock');
+const scissors = document.querySelector('.game-scissors');
+
+const signList = [];
 
 
+// sign onclick functions
 
 function choosePaper() {
     choice.style.display = 'none';
     rockContainer.style.display = 'none';
     part1.style.display = 'flex';
     player.classList.add('game-paper');
+
+    setTimeout(function() {
+        let random = Math.floor(Math.random() * 3);
+
+        if (random = 0) {
+            housePick.classList.add('.base-class', '.game-scissors');
+        } else if (random = 1) {
+            housePick.classList.add('.base-class', '.game-rock');
+        } else {
+            housePick.classList.add('.base-class', '.game-paper');
+        }
+
+    }, 2000);
 }
 
 function chooseScissors() {
@@ -29,4 +48,20 @@ function chooseRock() {
     rockContainer.style.display = 'none';
     part1.style.display = 'flex';
     player.classList.add('game-rock');
+
+}
+
+// computer pick
+
+function housePick() {
+    let random = Math.floor(Math.random() * 3);
+
+    if (random = 0) {
+        housePick.classList.add('.base-class', '.game-scissors');
+    } else if (random = 1) {
+        housePick.classList.add('.base-class', '.game-rock');
+    } else {
+        housePick.classList.add('.base-class', '.game-paper');
+    }
+
 }
